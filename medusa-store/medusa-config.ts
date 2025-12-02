604197fb290ecd053495a60b6aa09c9256647acf
Yoyo99 <lauboin@free.fr>
2025-12-02 17:13:31 +0100

    Remove host and port configuration from Medusa

--- medusa-store/medusa-config.ts
+++ medusa-store/medusa-config.ts
@@ -11,10 +11,6 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      // 👇 C'EST ICI QUE LA MAGIE OPÈRE :
-      // On dit à Medusa d'utiliser les variables qu'on a mises dans Coolify
-      host: process.env.HOST || "0.0.0.0",
-      port: process.env.PORT ? parseInt(process.env.PORT) : 9000
     }
   }
 })
