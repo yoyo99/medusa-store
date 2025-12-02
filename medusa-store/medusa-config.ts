abe4e86b671f9ba008afa96233e3a65d5649329f
Yoyo99 <lauboin@free.fr>
2025-12-02 17:58:25 +0100

    Add database driver options to config

--- medusa-store/medusa-config.ts
+++ medusa-store/medusa-config.ts
@@ -5,6 +5,11 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+     databaseDriverOptions: {
+      connection: {
+        ssl: false
+      }
+    },
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
